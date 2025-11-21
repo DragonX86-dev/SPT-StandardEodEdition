074f452d07363c792deae1a5e56a224b40b1441d
DenisZhmakin <den.zhmakin.00@gmail.com>
2025-11-21 15:06:38 +0300

    feat: Добавил DescriptionLocaleKey для профиля.

--- StandardEoDEdition/StandardEodEditionExtension.cs
+++ StandardEoDEdition/StandardEodEditionExtension.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
-using System.Text.Json;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Helpers;
 using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Servers;
+using SPTarkov.Server.Core.Services;
 using Path = System.IO.Path;
 
 namespace StandardEoDEdition;
@@ -13,56 +13,58 @@ namespace StandardEoDEdition;
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
 public class StandardEodEditionExtension(
     ModHelper modHelper,
+    LocaleService localeService,
     DatabaseServer databaseServer,
     ISptLogger<StandardEodEditionExtension> logger) : IOnLoad
 {
     public Task OnLoad()
     {
+        var serverLocale = localeService.GetDesiredServerLocale();
         var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        var eodProfileCopy = databaseServer.GetTables().Templates.Profiles["Edge Of Darkness"] with
+        {
+            DescriptionLocaleKey = serverLocale switch
+            {
+                "en" => "Standard edition with EoD chips",
+                "ru" => "Стандартное издание с фишками из EoD",
+                _ => ""
+            }
+        };
 
-        var eodProfileDeepCopy = DeepCopy(databaseServer.GetTables().Templates.Profiles["Edge Of Darkness"]);
-
-        if (eodProfileDeepCopy.Bear?.Character?.Hideout is null 
-            || eodProfileDeepCopy.Usec?.Character?.Hideout is null)
+        if (eodProfileCopy.Bear?.Character?.Hideout is null 
+            || eodProfileCopy.Usec?.Character?.Hideout is null)
         {
             logger.Error("The profile cannot be copied");
             return Task.CompletedTask;
         }
         
-        eodProfileDeepCopy.Bear.Character.Inventory = modHelper.GetJsonDataFromFile<BotBaseInventory>(
+        eodProfileCopy.Bear.Character.Inventory = modHelper.GetJsonDataFromFile<BotBaseInventory>(
             pathToMod, Path.Combine("data", "bear_inventory.json"));
-        eodProfileDeepCopy.Usec.Character.Inventory = modHelper.GetJsonDataFromFile<BotBaseInventory>(
+        eodProfileCopy.Usec.Character.Inventory = modHelper.GetJsonDataFromFile<BotBaseInventory>(
             pathToMod, Path.Combine("data", "usec_inventory.json"));
         
-        eodProfileDeepCopy.Bear.Character.Skills = modHelper.GetJsonDataFromFile<Skills>(
+        eodProfileCopy.Bear.Character.Skills = modHelper.GetJsonDataFromFile<Skills>(
             pathToMod, Path.Combine("data", "skill_issue.json"));
-        eodProfileDeepCopy.Usec.Character.Skills = modHelper.GetJsonDataFromFile<Skills>(
+        eodProfileCopy.Usec.Character.Skills = modHelper.GetJsonDataFromFile<Skills>(
             pathToMod, Path.Combine("data", "skill_issue.json"));
         
-        eodProfileDeepCopy.Bear.Character.Hideout.Areas = modHelper.GetJsonDataFromFile<List<BotHideoutArea>>(
+        eodProfileCopy.Bear.Character.Hideout.Areas = modHelper.GetJsonDataFromFile<List<BotHideoutArea>>(
             pathToMod, Path.Combine("data", "hideout_areas.json"));
-        eodProfileDeepCopy.Usec.Character.Hideout.Areas = modHelper.GetJsonDataFromFile<List<BotHideoutArea>>(
+        eodProfileCopy.Usec.Character.Hideout.Areas = modHelper.GetJsonDataFromFile<List<BotHideoutArea>>(
             pathToMod, Path.Combine("data", "hideout_areas.json"));
         
-        eodProfileDeepCopy.Bear.Character.Bonuses = modHelper.GetJsonDataFromFile<List<Bonus>>(
+        eodProfileCopy.Bear.Character.Bonuses = modHelper.GetJsonDataFromFile<List<Bonus>>(
             pathToMod, Path.Combine("data", "bonuses.json"));
-        eodProfileDeepCopy.Usec.Character.Bonuses = modHelper.GetJsonDataFromFile<List<Bonus>>(
+        eodProfileCopy.Usec.Character.Bonuses = modHelper.GetJsonDataFromFile<List<Bonus>>(
             pathToMod, Path.Combine("data", "bonuses.json"));
         
-        eodProfileDeepCopy.Bear.Trader = modHelper.GetJsonDataFromFile<ProfileTraderTemplate>(
+        eodProfileCopy.Bear.Trader = modHelper.GetJsonDataFromFile<ProfileTraderTemplate>(
             pathToMod, Path.Combine("data", "traders.json"));
-        eodProfileDeepCopy.Usec.Trader = modHelper.GetJsonDataFromFile<ProfileTraderTemplate>(
+        eodProfileCopy.Usec.Trader = modHelper.GetJsonDataFromFile<ProfileTraderTemplate>(
             pathToMod, Path.Combine("data", "traders.json"));
-
-        eodProfileDeepCopy.DescriptionLocaleKey = "Standard edition with EoD chips";
-
-        databaseServer.GetTables().Templates.Profiles["Standard Eod Edition"] = eodProfileDeepCopy;
+        
+        databaseServer.GetTables().Templates.Profiles["Standard Eod Edition"] = eodProfileCopy;
 
         return Task.CompletedTask;
-    }
-
-    private static T DeepCopy<T>(T obj)
-    {
-        return JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(obj))!;
     }
 }
